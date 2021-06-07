9178a40c3901f1d772960b4aae2eade17d206e9e
villysiu <villysiu@gmail.com>
2021-06-07 09:35:10 -0700

    commit tree a6a45502d0054b0f2803d8bae76882ccd6c891c6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function fetchToys() {
+  // To pass the tests, don't forget to return your fetch!
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(data => renderToys(data));
+  
+}
+
+function renderToys(toys) {
+  const main = document.querySelector('#toy-collection');
+  toys.forEach(toy => {
+    const div = document.createElement('div');
+    div.className = 'card';
+    div.innerHTML = toy.name;
+    main.appendChild(div);
+  });
+}
+
+
+
+
+
+
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +36,5 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetchToys();
 });
